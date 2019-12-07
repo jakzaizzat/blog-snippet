8d3f174934258ea70b169f8d9434bcbc57d0fd01
Jakz Aizzat <jakzaizzat@gmail.com>
2019-12-07 19:55:58 +0800

    Install Vue Idle

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Home from "./views/Home"
+import IdleTimer from "./views/IdleTimer";
 
 export default new Router({
     mode: 'history',
@@ -12,6 +13,11 @@ export default new Router({
             path: "/",
             name: 'Home',
             component: Home
+        },
+        {
+            path: "/idle",
+            name: "Idle Timer",
+            component: IdleTimer
         }
     ]
 })
---
+++ src/views/IdleTimer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Idle Timer Page
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "IdleTimer"
+    }
+</script>
+
+<style scoped>
+
+</style>
