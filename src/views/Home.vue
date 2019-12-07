de8a849f92b38afbbb2335f40514ae26018e4032
Jakz Aizzat <jakzaizzat@gmail.com>
2019-12-07 19:47:09 +0800

    Initialize

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <HelloWorld/>
+    </div>
+</template>
+
+<script>
+    import HelloWorld from "../components/HelloWorld";
+    export default {
+        components: {
+            HelloWorld
+        }
+    }
+</script>
