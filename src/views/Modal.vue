00caf7ff5ce283ee1cab67ce61d4bd1323561687
Jakz Aizzat <jakzaizzat@gmail.com>
2020-02-16 11:15:07 +0800

    Add modal component

--- src/views/Modal.vue
+++ src/views/Modal.vue
@@ -1,5 +1,53 @@
 <template>
-    <div>Modal Page</div>
+  <div class="overlay">
+    <div class="modal">
+      <div class="flex items-center justify-between p-3 font-bold">
+        <span>Modal Title</span>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              class="heroicon-ui"
+              d="M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="p-3">
+          <div class="mb-4">Modal Body</div>
+          <button class="bg-blue-500 text-white p-2 text-sm rounded">Confirm</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+};
+</script>
+
+<style lang="postcss" scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  width: 500px;
+  box-shadow: 1px 2px 4px rgba(153, 155, 168, 0.12);
+  border-radius: 4px;
+  @apply bg-white p-2;
+}
+</style>
